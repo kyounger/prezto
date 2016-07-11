70aec672a73afdd163656ca94c3ef5cc455f6007
Kenny Younger <kyounger@gmail.com>
2016-07-11 11:52:50 -0500

    Add sdkman

---
+++ modules/sdkman/init.zsh
@@ -0,0 +1,3 @@
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="/Users/kyounger/.sdkman"
+[[ -s "/Users/kyounger/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/kyounger/.sdkman/bin/sdkman-init.sh"
